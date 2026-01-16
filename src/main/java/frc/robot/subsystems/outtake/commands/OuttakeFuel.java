1677eef988c7ff77f49eb59dfa08164021a362be
Alexander <9071454@philasd.org>
2026-01-16 16:18:09 -0500

    feat: changed command parameters to suppliers

--- src/main/java/frc/robot/subsystems/outtake/commands/OuttakeFuel.java
+++ src/main/java/frc/robot/subsystems/outtake/commands/OuttakeFuel.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.outtake.commands;
 
+import java.util.function.Supplier;
+
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -8,36 +10,36 @@ import frc.robot.subsystems.outtake.OuttakeConstants;
 
 public class OuttakeFuel {
     
-public static Command outtakeWithVoltage(Outtake outtake, Voltage volts){
-    return outtake.runVolts(volts);
+public static Command outtakeWithVoltage(Outtake outtake, Supplier<Voltage> volts){
+    return outtake.runVolts(volts.get());
 }
 
-public static Command outtakeWithVelocity(Outtake outtake, AngularVelocity rpm){
-    return outtake.setControl(rpm);
+public static Command outtakeWithVelocity(Outtake outtake, Supplier<AngularVelocity> rpm){
+    return outtake.setControl(rpm.get());
 }
 
 public static Command feedNeutralZone(Outtake outtake){
-    return outtakeWithVelocity(outtake, OuttakeConstants.kNeutralFeedRPM);
+    return outtakeWithVelocity(outtake, ()-> OuttakeConstants.kNeutralFeedRPM);
 }
 
 public static Command feedOppositeZone(Outtake outtake){
-    return outtakeWithVelocity(outtake, OuttakeConstants.kOppositeFeedRPM);
+    return outtakeWithVelocity(outtake, ()-> OuttakeConstants.kOppositeFeedRPM);
 }
 
 public static Command scoreRegion1(Outtake outtake){
-    return outtakeWithVelocity(outtake, OuttakeConstants.kRegion1ScoreRPM);
+    return outtakeWithVelocity(outtake, ()-> OuttakeConstants.kRegion1ScoreRPM);
 }
 
 public static Command scoreRegion2(Outtake outtake){
-    return outtakeWithVelocity(outtake, OuttakeConstants.kRegion2ScoreRPM);
+    return outtakeWithVelocity(outtake, ()-> OuttakeConstants.kRegion2ScoreRPM);
 }
 
 public static Command scoreRegion3(Outtake outtake){
-    return outtakeWithVelocity(outtake, OuttakeConstants.kRegion3ScoreRPM);
+    return outtakeWithVelocity(outtake, ()-> OuttakeConstants.kRegion3ScoreRPM);
 }
 
 public static Command releaseFuel(Outtake outtake){
-    return outtakeWithVelocity(outtake, OuttakeConstants.kReleaseRPM);
+    return outtakeWithVelocity(outtake, ()-> OuttakeConstants.kReleaseRPM);
 }
 
 }
