e6bb3d7e15f91ad86955e41ddc2ec5314e4a2e6e
Alexander <9071454@philasd.org>
2026-01-23 19:20:12 -0500

    Applied spotless

--- src/main/java/frc/robot/subsystems/outtake/OuttakeConstants.java
+++ src/main/java/frc/robot/subsystems/outtake/OuttakeConstants.java
@@ -1,3 +1,4 @@
+/* (C) RoboLancers 2026 */
 package frc.robot.subsystems.outtake;
 
 import static edu.wpi.first.units.Units.RPM;
@@ -5,29 +6,28 @@ import static edu.wpi.first.units.Units.RPM;
 import edu.wpi.first.units.measure.AngularVelocity;
 
 public class OuttakeConstants {
-    
-public static final int kMotorID = 0;
 
-public static final double kStatorLimit = 40;
+  public static final int kMotorID = 0;
 
-public static final boolean kInverted = false;
+  public static final double kStatorLimit = 40;
 
-public static final double kGearing = 1;
+  public static final boolean kInverted = false;
 
-public static final double kNominalVoltage = 12;
+  public static final double kGearing = 1;
 
-public static final AngularVelocity kNeutralFeedRPM = RPM.of(0);
+  public static final double kNominalVoltage = 12;
 
-public static final AngularVelocity kOppositeFeedRPM = RPM.of(0);
+  public static final AngularVelocity kNeutralFeedRPM = RPM.of(0);
 
-public static final AngularVelocity kRegion1ScoreRPM = RPM.of(0);
+  public static final AngularVelocity kOppositeFeedRPM = RPM.of(0);
 
-public static final AngularVelocity kRegion2ScoreRPM = RPM.of(0);
+  public static final AngularVelocity kRegion1ScoreRPM = RPM.of(0);
 
-public static final AngularVelocity kRegion3ScoreRPM = RPM.of(0);
+  public static final AngularVelocity kRegion2ScoreRPM = RPM.of(0);
 
-public static final AngularVelocity kSetPositionScoreRPM = RPM.of(0);
+  public static final AngularVelocity kRegion3ScoreRPM = RPM.of(0);
 
-public static final AngularVelocity kReleaseRPM = RPM.of(0);
+  public static final AngularVelocity kSetPositionScoreRPM = RPM.of(0);
 
+  public static final AngularVelocity kReleaseRPM = RPM.of(0);
 }
--- src/main/java/frc/robot/subsystems/outtake/commands/OuttakeFuel.java
+++ src/main/java/frc/robot/subsystems/outtake/commands/OuttakeFuel.java
@@ -1,49 +1,48 @@
+/* (C) RoboLancers 2026 */
 package frc.robot.subsystems.outtake.commands;
-
-import java.util.function.Supplier;
 
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.outtake.Outtake;
 import frc.robot.subsystems.outtake.OuttakeConstants;
+import java.util.function.Supplier;
 
 public class OuttakeFuel {
-    
-public static Command outtakeWithVoltage(Outtake outtake, Supplier<Voltage> volts){
+
+  public static Command outtakeWithVoltage(Outtake outtake, Supplier<Voltage> volts) {
     return outtake.runVolts(volts.get());
-}
+  }
 
-public static Command outtakeWithVelocity(Outtake outtake, Supplier<AngularVelocity> rpm){
+  public static Command outtakeWithVelocity(Outtake outtake, Supplier<AngularVelocity> rpm) {
     return outtake.setControl(rpm.get());
-}
+  }
 
-public static Command feedNeutralZone(Outtake outtake){
-    return outtakeWithVelocity(outtake, ()-> OuttakeConstants.kNeutralFeedRPM);
-}
+  public static Command feedNeutralZone(Outtake outtake) {
+    return outtakeWithVelocity(outtake, () -> OuttakeConstants.kNeutralFeedRPM);
+  }
 
-public static Command feedOppositeZone(Outtake outtake){
-    return outtakeWithVelocity(outtake, ()-> OuttakeConstants.kOppositeFeedRPM);
-}
+  public static Command feedOppositeZone(Outtake outtake) {
+    return outtakeWithVelocity(outtake, () -> OuttakeConstants.kOppositeFeedRPM);
+  }
 
-public static Command scoreRegion1(Outtake outtake){
-    return outtakeWithVelocity(outtake, ()-> OuttakeConstants.kRegion1ScoreRPM);
-}
+  public static Command scoreRegion1(Outtake outtake) {
+    return outtakeWithVelocity(outtake, () -> OuttakeConstants.kRegion1ScoreRPM);
+  }
 
-public static Command scoreRegion2(Outtake outtake){
-    return outtakeWithVelocity(outtake, ()-> OuttakeConstants.kRegion2ScoreRPM);
-}
+  public static Command scoreRegion2(Outtake outtake) {
+    return outtakeWithVelocity(outtake, () -> OuttakeConstants.kRegion2ScoreRPM);
+  }
 
-public static Command scoreRegion3(Outtake outtake){
-    return outtakeWithVelocity(outtake, ()-> OuttakeConstants.kRegion3ScoreRPM);
-}
+  public static Command scoreRegion3(Outtake outtake) {
+    return outtakeWithVelocity(outtake, () -> OuttakeConstants.kRegion3ScoreRPM);
+  }
 
-public static Command scoreSetPosition(Outtake outtake){
-    return outtakeWithVelocity(outtake, ()-> OuttakeConstants.kSetPositionScoreRPM);
-}
+  public static Command scoreSetPosition(Outtake outtake) {
+    return outtakeWithVelocity(outtake, () -> OuttakeConstants.kSetPositionScoreRPM);
+  }
 
-public static Command releaseFuel(Outtake outtake){
-    return outtakeWithVelocity(outtake, ()-> OuttakeConstants.kReleaseRPM);
-}
-
+  public static Command releaseFuel(Outtake outtake) {
+    return outtakeWithVelocity(outtake, () -> OuttakeConstants.kReleaseRPM);
+  }
 }
