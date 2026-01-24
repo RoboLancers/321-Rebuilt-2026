beec1d27e3f4befdaab6336aa72e31aa85bfb425
Alexander <9071454@philasd.org>
2026-01-24 11:33:57 -0500

    Changed command input to suppliers

--- src/main/java/frc/robot/subsystems/indexer/indexerCommands/TuneIndexer.java
+++ src/main/java/frc/robot/subsystems/indexer/indexerCommands/TuneIndexer.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.indexer.Indexer;
 
-public class Tune {
+public class TuneIndexer {
 
   public static Command tune(Indexer indexer) {
     return Commands.run(() -> indexer.tune());
