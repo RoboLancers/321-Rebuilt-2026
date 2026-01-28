d5907221ffd2e70278ca0d32f0b5783718e32e10
Alexander <9071454@philasd.org>
2026-01-28 12:54:06 -0500

    Removed unmerged vision subsystem

--- src/main/java/frc/robot/subsystems/vision/VisionEstimate.java
+++
@@ -1,6 +0,0 @@
-/* (C) RoboLancers 2026 */
-package frc.robot.subsystems.vision;
-
-import org.photonvision.EstimatedRobotPose;
-
-public record VisionEstimate(EstimatedRobotPose estimatedPose, double standardDeviations) {}
