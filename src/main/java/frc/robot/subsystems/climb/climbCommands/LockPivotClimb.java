7da4facf216fb3bf7b9aad5ca5a7ac82d5a679b9
MichaelLi1234 <8633730@philasd.org>
2026-02-02 18:41:08 -0500

    applied spotless

--- src/main/java/frc/robot/subsystems/climb/climbCommands/LockPivotClimb.java
+++ src/main/java/frc/robot/subsystems/climb/climbCommands/LockPivotClimb.java
@@ -1,27 +1,27 @@
+/* (C) RoboLancers 2026 */
 package frc.robot.subsystems.climb.climbCommands;
 
 import static edu.wpi.first.units.Units.Volts;
 
 import com.ctre.phoenix6.hardware.TalonFX;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.climb.Climb;
 
-public class LockPivotClimb extends Command{
+public class LockPivotClimb extends Command {
 
-    Climb climb;
-    TalonFX climbMotor;
+  Climb climb;
+  TalonFX climbMotor;
 
-    public LockPivotClimb(Climb climb, TalonFX climbMotor){
-        this.climb = climb;
-        this.climbMotor = climbMotor;
-    }
-    
-    public void execute(){
-        climbMotor.setVoltage(0);
-    }
+  public LockPivotClimb(Climb climb, TalonFX climbMotor) {
+    this.climb = climb;
+    this.climbMotor = climbMotor;
+  }
 
-    public boolean isFinished(){
-        return climb.atPivotClimbVoltage(Volts.of(0));
-    }
+  public void execute() {
+    climbMotor.setVoltage(0);
+  }
+
+  public boolean isFinished() {
+    return climb.atPivotClimbVoltage(Volts.of(0));
+  }
 }
