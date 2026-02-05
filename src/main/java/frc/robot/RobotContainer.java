d5df9f4f26b1a9e585babbf6345f2aa605c7c911
Alexander <9071454@philasd.org>
2026-02-04 21:16:33 -0500

    Reverted robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -45,60 +45,12 @@ import frc.robot.util.RebuiltUtil;
 
 public class RobotContainer {
 
-  public CommandXboxController driver = new CommandXboxController(0);
   public Drivetrain drivetrain = Drivetrain.create();
-
-  public Vision vision =
-      Vision.create(
-          est ->
-              drivetrain.addVisionMeasurement(
-                  est.estimatedPose().estimatedPose.toPose2d(),
-                  est.estimatedPose().timestampSeconds,
-                  VecBuilder.fill(
-                      est.standardDeviations(),
-                      est.standardDeviations(),
-                      est.standardDeviations())));
-
   private final SendableChooser<Command> autoChooser;
   private final IntakeRollers intakeRollers = new IntakeRollers();
   private final IntakeFuel intakeFuel = new IntakeFuel(intakeRollers);
   private final Shooter shooter = new Shooter();
   private final ShootFuel shootFuel = new ShootFuel();
-  private final Hood hood = new Hood();
-  private final IntakePivot intakePivot = new IntakePivot();
-  private final Indexer spindexer = new Indexer();
-  private final Tunnel tunnel = new Tunnel();
-
-  public Trigger slowMode = driver.b();
-
-  private DoubleSupplier driverForward =
-      () ->
-          -MathUtil.applyDeadband(
-                  Math.pow(Math.hypot(driver.getLeftY(), driver.getLeftX()), 2),
-                  DrivetrainConstants.kDriveDeadband)
-              * Math.cos(Math.atan2(driver.getLeftX(), driver.getLeftY()))
-              * (slowMode.getAsBoolean()
-                  ? 1.5
-                  : DrivetrainConstants.kMaxLinearVelocity.in(MetersPerSecond));
-
-  private DoubleSupplier driverStrafe =
-      () ->
-          -MathUtil.applyDeadband(
-                  Math.pow(Math.hypot(driver.getLeftY(), driver.getLeftX()), 2),
-                  DrivetrainConstants.kDriveDeadband)
-              * Math.sin(Math.atan2(driver.getLeftX(), driver.getLeftY()))
-              * (slowMode.getAsBoolean()
-                  ? 1.5
-                  : DrivetrainConstants.kMaxLinearVelocity.in(MetersPerSecond));
-
-  private DoubleSupplier driverTurn =
-      () ->
-          -MathUtil.applyDeadband(driver.getRightX(), DrivetrainConstants.kRotationDeadband)
-              * DrivetrainConstants.kMaxAngularVelocity.in(RadiansPerSecond);
-
-    private Supplier<Pose2d> currentRobotPose = ()->drivetrain.getPose();
-
-    private Supplier<Rotation2d> hubHeading = ()->RebuiltUtil.getHubHeading(currentRobotPose);
 
   public RobotContainer() {
     configureBindings();
@@ -110,20 +62,7 @@ public class RobotContainer {
     NamedCommands.registerCommand("ShootFuel", shootFuel.releaseFuel(shooter));
   }
 
-  private void configureBindings() {
-    drivetrain.setDefaultCommand(drivetrain.teleopDrive(driverForward, driverStrafe, driverTurn));
-    intakeRollers.setDefaultCommand(Commands.run(()->intakeRollers.setVoltage(0)));
-    shooter.setDefaultCommand(ShootFuel.outtakeWithVoltage(shooter, ()->Volts.of(0)));
-    hood.setDefaultCommand(HoodCommands.goToTravelAngle(hood));
-    intakePivot.setDefaultCommand(new GoToDefaultPosition(intakePivot));
-    spindexer.setDefaultCommand(Index.setVoltage(spindexer, ()->Volts.of(0)));
-    tunnel.setDefaultCommand(new RunAtVelocity(tunnel, RPM.of(0)));
-
-    driver.leftBumper().whileTrue(new GoToIntakePosition(intakePivot).andThen(new IntakeFuel(intakeRollers)));
-    driver.leftTrigger().whileTrue(Align.rotateToHubWhileDriving(drivetrain, driverForward, driverStrafe, hubHeading, currentRobotPose));
-    driver.rightTrigger().whileTrue(Score.shootFuelFromAnywhere(drivetrain, shooter, hood, spindexer, tunnel, currentRobotPose));
-    driver.rightBumper().whileTrue(Score.feedFuel(shooter,hood,spindexer,tunnel));
-  }
+  private void configureBindings() {}
 
   public Command getAutonomousCommand() {
     return autoChooser.getSelected();
