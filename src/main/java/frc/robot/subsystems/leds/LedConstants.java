dec3169a624c3248f63313c887c1d476952d705f
Jaely <10117985@philasd.org>
2026-02-06 17:38:50 -0500

    Added configs for leds

---
+++ src/main/java/frc/robot/subsystems/leds/LedConstants.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.leds;
+
+import com.ctre.phoenix6.signals.StripTypeValue;
+
+public class LedConstants {
+    public static final int candleId = 0;
+    public StripTypeValue stripTypeValue = StripTypeValue.GRB;
+    public static final int brightnessScaler = 0;
+}
--- src/main/java/frc/robot/subsystems/leds/Leds.java
+++ src/main/java/frc/robot/subsystems/leds/Leds.java
@@ -1,5 +1,30 @@
 package frc.robot.subsystems.leds;
 
+import com.ctre.phoenix6.configs.CANdleFeaturesConfigs;
+import com.ctre.phoenix6.configs.LEDConfigs;
+import com.ctre.phoenix6.hardware.CANdle;
+import com.ctre.phoenix6.signals.VBatOutputModeValue;
+import com.fasterxml.jackson.annotation.JsonFormat.Feature;
+
 public class Leds {
-    
+
+    private static Leds instance;
+    public final int ledPort = 0;
+    public final int ledStart = 0;
+    public final int ledEnd = 7;
+
+    public CANdle candle = new CANdle(0);
+
+    public Leds(){
+        LedConfigs();
+    }
+
+    public void LedConfigs(){
+        LEDConfigs configs = new LEDConfigs();
+        CANdleFeaturesConfigs featuresConfigs = new CANdleFeaturesConfigs();
+        configs.BrightnessScalar = LedConstants.brightnessScaler;
+        featuresConfigs.VBatOutputMode = VBatOutputModeValue.On;
+        candle.getConfigurator().apply(configs);
+        candle.getConfigurator().apply(featuresConfigs);
+    }
 }
