09a26d78b9ad3bd17d258e748d1e151e9ce951c7
MichaelLi1234 <8633730@philasd.org>
2026-03-06 22:57:40 -0500

    Added temporary intake command using voltage

---
+++ src/main/java/frc/robot/subsystems/intakerollers/rolllercommands/IntakeWithVoltage.java
@@ -0,0 +1,33 @@
+/* (C) RoboLancers 2026 */
+package frc.robot.subsystems.intakerollers.rolllercommands;
+
+import static edu.wpi.first.units.Units.Volts;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.intakerollers.IntakeRollerConstants;
+import frc.robot.subsystems.intakerollers.IntakeRollers;
+
+public class IntakeWithVoltage extends Command {
+
+  IntakeRollers intakeRollers;
+
+  public IntakeWithVoltage(IntakeRollers intakeRollers) {
+    this.intakeRollers = intakeRollers;
+    addRequirements(intakeRollers);
+  }
+
+  @Override
+  public void execute() {
+    intakeRollers.setVoltage(Volts.of(3));
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    intakeRollers.setVoltage(Volts.of(0));
+  }
+}
