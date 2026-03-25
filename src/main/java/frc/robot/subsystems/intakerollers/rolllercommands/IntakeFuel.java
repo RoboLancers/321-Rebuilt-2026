018d9891c77b057ea55f558e2ed63c1668df34e4
MichaelLi1234 <8633730@philasd.org>
2026-03-25 17:22:01 -0400

    Made IntakeFuel command require an angle as to not interfere with the net

--- src/main/java/frc/robot/subsystems/intakerollers/rolllercommands/IntakeFuel.java
+++ src/main/java/frc/robot/subsystems/intakerollers/rolllercommands/IntakeFuel.java
@@ -1,19 +1,23 @@
 /* (C) RoboLancers 2026 */
 package frc.robot.subsystems.intakerollers.rolllercommands;
 
+import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.RPM;
 import static edu.wpi.first.units.Units.Volts;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.intakePivot.IntakePivot;
 import frc.robot.subsystems.intakerollers.IntakeRollerConstants;
 import frc.robot.subsystems.intakerollers.IntakeRollers;
 
 public class IntakeFuel extends Command {
 
   IntakeRollers intakeRollers;
+  IntakePivot intakePivot;
 
-  public IntakeFuel(IntakeRollers intakeRollers) {
+  public IntakeFuel(IntakeRollers intakeRollers, IntakePivot intakePivot) {
     this.intakeRollers = intakeRollers;
+    this.intakePivot = intakePivot;
     addRequirements(intakeRollers);
   }
 
@@ -24,7 +28,9 @@ public class IntakeFuel extends Command {
 
   @Override
   public void execute() {
-    intakeRollers.goToVelocity(IntakeRollerConstants.kIntakeFuelVelocity);
+    if (intakePivot.getAngle().in(Degrees) < 30) {
+      intakeRollers.goToVelocity(IntakeRollerConstants.kIntakeFuelVelocity);
+    }
   }
 
   @Override
