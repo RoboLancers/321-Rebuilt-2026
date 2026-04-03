77ae936bd78ec06d688e6cd587e394dfcd4f9a7f
MichaelLi1234 <8633730@philasd.org>
2026-04-03 13:28:36 -0400

    added controls from defense mdoe to philly

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -79,16 +79,6 @@ public class RobotContainer {
   private SendableChooser<Command> autoChooser;
 
   public Trigger slowMode = driver.b();
-  private boolean defenseMode = false;
-
-  @Logged
-  public boolean getDefenseMode() {
-    return defenseMode;
-  }
-
-  public void toggleDefenseMode() {
-    defenseMode = !defenseMode;
-  }
 
   @Logged(name = "driverForwardValue")
   public double getDriverForward() {
@@ -124,26 +114,6 @@ public class RobotContainer {
         -MathUtil.applyDeadband(driver.getRightX(), DrivetrainConstants.kRotationDeadband)
             * DrivetrainConstants.kMaxAngularVelocity.in(RadiansPerSecond);
     return rawJoystick;
-  }
-
-  @Logged(name = "forwardVelocityValue")
-  public double getForwardVelocity() {
-    double forwardVelocity =
-        (getDefenseMode() && !RebuiltUtil.inDefenseZone(drivetrain.getPose()))
-            ? MathUtil.clamp(
-                getDriverForward(), 0, DrivetrainConstants.kMaxLinearVelocity.in(MetersPerSecond))
-            : getDriverForward();
-    return forwardVelocity;
-  }
-
-  @Logged(name = "strafeVelocityValue")
-  public double getStrafeVelocity() {
-    return getDriverStrafe();
-  }
-
-  @Logged(name = "turnVelocityValue")
-  public double getTurnVelocity() {
-    return getDriverTurn();
   }
 
   @Logged(name = "calculatedHubHeading")
@@ -261,11 +231,7 @@ public class RobotContainer {
     shooter.setDefaultCommand(new ShooterDefaultBehavior(shooter, drivetrain::getPose));
 
     drivetrain.setDefaultCommand(
-        drivetrain.defenseDrive(
-            this::getForwardVelocity,
-            this::getStrafeVelocity,
-            this::getTurnVelocity,
-            this::getDefenseMode));
+        drivetrain.teleopDrive(this::getDriverForward, this::getDriverStrafe, this::getDriverTurn));
 
     driver
         .leftBumper()
@@ -314,23 +280,14 @@ public class RobotContainer {
 
     driver.a().whileTrue(new StaticShoot(tunnel, shooter, indexer, hood));
     driver.b().whileTrue(new Feed(tunnel, shooter, hood, indexer));
-
     driver
         .x()
-        .onTrue(
-            Commands.runOnce(
-                () -> {
-                  toggleDefenseMode();
-                }));
-
-    driver.povLeft().whileTrue(new Release(tunnel, shooter, indexer));
-    driver
-        .povRight()
         .whileTrue(
             new SetIntakeVelocity(
                     intakeRollers, intakePivot, () -> IntakeRollerConstants.kReleaseVelocity)
                 .alongWith(
                     new SetIndexerVelocity(indexer, () -> IndexerConstants.kReleaseVelocity)));
+    driver.povLeft().whileTrue(new Release(tunnel, shooter, indexer));
     driver.povUp().onTrue(Commands.runOnce(() -> drivetrain.getPigeon2().setYaw(0)));
   }
 
