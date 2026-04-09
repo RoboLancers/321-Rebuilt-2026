56e0647413c7013970dae4f986693154e1f1c94e
Alex Peng <9071454@philasd.org>
2026-04-09 07:46:22 -0400

    Changed intake pivot default command to zero volts because the pulley is currently rounded out

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -273,8 +273,7 @@ public class RobotContainer {
     intakeRollers.setDefaultCommand(
         Commands.run(() -> intakeRollers.setVoltage(Volts.of(0)), intakeRollers));
     indexer.setDefaultCommand(new SetIndexerVelocity(indexer, () -> RPM.of(0)));
-    intakePivot.setDefaultCommand(
-        new GoToAnglePersist(intakePivot, () -> IntakeConstants.kStowedPosition));
+    intakePivot.setDefaultCommand(Commands.run(()->intakePivot.setVoltage(Volts.of(0)), intakePivot));
     hood.setDefaultCommand(Commands.run(() -> hood.runVolts(Volts.of(0)), hood));
     // shooter.setDefaultCommand(new SetShooterVelocity(shooter, () -> RPM.of(0)));
     shooter.setDefaultCommand(new SetShooterVelocity(shooter, () -> RPM.of(0)));
